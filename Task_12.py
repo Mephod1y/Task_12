d494c652eae15aea6bee2a46304aad3034ead8ab
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-12-15 15:34:09 +0200

    Add files via upload

---
+++ Task_12.py
@@ -0,0 +1,135 @@
+from Task_12_classes import AddressBook, Record
+
+contacts_dict = AddressBook()
+
+def input_error(function):
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except KeyError:
+            return 'Wrong name'
+        except ValueError as exception:
+            return exception.args[0]
+        except IndexError:
+            return 'Enter name and number'
+        except TypeError:
+            return 'Wrong command'
+    return wrapper
+
+@input_error
+def hello():
+    return 'How can I help you?'
+
+@input_error
+def exit_func():
+    return "Good bye"
+
+@input_error
+def show_all():  # функція повертає всі записи в словнику AddressBook
+    contacts = "All data in AddressBook:"
+    for name in contacts_dict:
+        contacts += f'\n{name}: '
+        for phone in contacts_dict[name].phones:
+            contacts += f"{phone.value}"
+    return contacts
+
+@input_error
+def add_name_phone(data):  # функція додає ведене ім'я телефон до екземляру класу Record
+    new_data = data.strip().split(" ")
+    name = new_data[0]
+    phones = new_data[1]
+    if name in contacts_dict:
+        raise ValueError('This contact already exist.')
+    record = Record(name)
+    record.put_phone_list(phones)
+    contacts_dict.add_record(record)
+    return f'You added new contact: {name} with this {phones}'
+
+@input_error
+def change_phone(user_input):  # функція змінює телефон по ключу в словнику AddressBook
+    user_input = user_input.split()
+    name = user_input[0]
+    current_phone = user_input[1]
+    new_phone = user_input[2]
+    return contacts_dict[name].change(current_phone, new_phone)
+
+@input_error
+def show_phone(user_input): # функція повертає телефон по ключу в словнику AddressBook
+    phones = ""
+    user_input = user_input.split()
+    name = user_input[0]
+    for phone in (contacts_dict[name]).phones:
+        phones += f"{phone.value} "
+    return phones
+
+@input_error
+def add_birth(user_input):
+    user_input = user_input.split()
+    name = user_input[0]
+    birthday = user_input[1]
+    return contacts_dict[name].add_birthday(birthday)
+
+@input_error
+def show_birth(user_input):
+    user_input = user_input.split()
+    name = user_input[0]
+    return contacts_dict[name].days_to_birthday()
+
+@input_error
+def search(user_input):
+    user_input = user_input.split()
+    name = user_input[0]
+    return contacts_dict.search(name)
+
+def save():
+    return contacts_dict.save_to_file()
+
+def read():
+    return contacts_dict.read_from_file()
+
+functions = {
+    'hello': hello,
+    'add': add_name_phone,
+    'change': change_phone,
+    'phone': show_phone,
+    'show all': show_all,
+    'exit': exit_func,
+    'close': exit_func,
+    'good bye': exit_func,
+    'birth': add_birth,
+    'day': show_birth,
+    'search': search,
+    'save': save,
+    'read': read
+}
+
+def parser_input(user_input):
+    new_input = user_input
+    data = ''
+    for key in functions:
+        if user_input.strip().lower().startswith(key):
+            new_input = key
+            data = user_input[len(new_input):]
+            break
+    if data:
+        return reaction_func(new_input)(data)
+    return reaction_func(new_input)()
+
+def reaction_func(reaction):
+    return functions.get(reaction, break_func)
+
+def break_func():
+    return 'Wrong command'
+
+def main():
+    read()
+    while True:
+        user_input = input('Enter command: ')
+        result = parser_input(user_input)
+        print(result)
+        if result == 'Good bye':
+            break
+    save()
+
+if __name__ == "__main__":
+    main()
---
+++ Task_12_classes.py
@@ -0,0 +1,124 @@
+from collections import UserDict
+from datetime import datetime
+import re
+import pickle
+filename = 'Addressbook.dat'
+
+class Field():
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+       self._value = new_value
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+
+    @Field.value.setter
+    def value(self, new_value):
+        if bool(re.match('\d{3}.\d{3}.\d{2}.\d{2}', new_value)):
+            self._value = new_value
+        else:
+            raise ValueError("Input phone in format ***-***-**-**")
+
+class Birthday(Field):
+
+     @Field.value.setter
+     def value(self, new_value):
+         if bool(re.match('\d{4}[.]\d{2}[.]\d{2}', new_value)):
+             self._value = datetime.strptime(new_value, "%Y.%m.%d")
+         else:
+            raise ValueError("Input date in format YYYY.MM.DD")
+
+class Record():
+    def __init__(self, name, *phones):
+        self.name = Name(name)
+        self.phones = []
+        if phones:
+            for phone in phones:
+                self.put_phone_list(phone)
+        self.birthday = ''
+
+    def put_phone_list(self, phone_new):
+        phone_new = Phone(phone_new).value
+        for phone in self.phones:
+            if phone_new == phone.value:
+                print(f"{phone_new} already recorded for {self.name.value}")
+        self.phones.append(Phone(phone_new))
+
+    def change(self, phone_old, phone_new):
+
+        phone_old = Phone(phone_old).value
+        phone_new = Phone(phone_new).value
+        match = False
+
+        for phone in self.phones:
+
+            if phone.value == phone_new:
+                return f"{phone_new} already recorded for {self.name.value}"
+
+            if phone.value == phone_old:
+                match = True
+
+        if not match:
+            return f"{phone_old} is not exist in the contact {self.name.value}"
+
+        for index, phone in enumerate(self.phones):
+            if phone.value == phone_old:
+                self.phones.remove(phone)
+                self.phones.insert(index, Phone(phone_new))
+                return f"{phone_old} changed to {phone_new}"
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+        return f"{birthday} added to {self.name.value}"
+
+    def days_to_birthday(self):
+        today = datetime.today()
+        bday = self.birthday.value
+        timediff = (bday - today).days + 1 if (today - bday).days < 0 else (datetime(bday.year + 1, bday.month,bday.day) - today).days + 1
+        return f'{timediff} days till {self.name.value} birthday left!'
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def iterator(self, count=5):
+        page = []
+        i = 0
+        for record in self.data.values():
+            page.append(record)
+            i += 1
+            if i == count:
+                yield page
+                page = []
+                i = 0
+        if page:
+            yield page
+
+    def search(self, value):
+        if bool(self.data.get(value)):
+            return self.data.get(value).name
+        for record in self.data.values():
+            for phone in record.phones:
+                if phone.value == value:
+                    return record
+        raise ValueError("Contact with this value does not exist.")
+
+    def save_to_file(self):
+        with open(filename, "wb") as pack:
+            pickle.dump(self, pack)
+        return f'all data was recorded successfully'
+
+    def read_from_file(self):
+        with open(filename, "rb") as unpack:
+            contacts_dict = pickle.load(unpack)
+            return contacts_dict
